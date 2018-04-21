25c6cdde9dff0bb850d6bb367c662df616a8d9cc
Krzysztof Dzido <kdzido@mb-public.local>
2018-04-21 09:14:55 +0200

    - added failing test that should break pipelne

---
+++ backend-eureka-service/src/test/java/com/kdzido/thesis/eureka/SampleTest.java
@@ -0,0 +1,13 @@
+package com.kdzido.thesis.eureka;
+
+import org.junit.Test;
+
+import static org.hibernate.validator.internal.util.Contracts.assertTrue;
+
+public class SampleTest {
+
+    @Test
+    public void testTruth() {
+        assertTrue(false, "true");
+    }
+}
