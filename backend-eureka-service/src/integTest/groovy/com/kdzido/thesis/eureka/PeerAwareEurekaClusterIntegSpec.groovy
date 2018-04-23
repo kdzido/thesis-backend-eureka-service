ee57754365dbb921f467d18a7c6b105d8bc7f5f3
Krzysztof Dzido <kdzido@mb-public.local>
2018-04-23 17:39:30 +0200

    - added staging tests step to pipeline

--- backend-eureka-service/src/integTest/groovy/com/kdzido/thesis/eureka/PeerAwareEurekaClusterIntegSpec.groovy
+++ backend-eureka-service/src/integTest/groovy/com/kdzido/thesis/eureka/PeerAwareEurekaClusterIntegSpec.groovy
@@ -1,9 +1,11 @@
 package com.kdzido.thesis.eureka
 
 import io.restassured.http.ContentType
+import spock.lang.Ignore
 import spock.lang.Requires
 import spock.lang.Specification
 import spock.lang.Timeout
+import spock.lang.Unroll
 
 import java.util.concurrent.TimeUnit
 
@@ -14,11 +16,12 @@ import static org.hamcrest.Matchers.*
 /**
  * @author krzysztof.dzido@gmail.com
  */
-@Requires({env['EUREKASERVICE_URI_1'] && env['EUREKASERVICE_URI_2']})
+@Requires({ env['EUREKASERVICE_URI_1'] && env['EUREKASERVICE_URI_2'] })
 class PeerAwareEurekaClusterIntegSpec extends Specification {
 
-    @Timeout(unit=TimeUnit.MINUTES, value=5)
-    def "should create eureka cluster out of 2 peers"() {
+    @Unroll
+    @Timeout(unit = TimeUnit.MINUTES, value = 5)
+    def "should create eureka cluster out of peers: #peer1, #peer2"() {
         given:
         def peer1 = System.getenv("EUREKASERVICE_URI_1")
         def peer2 = System.getenv("EUREKASERVICE_URI_2")
@@ -30,21 +33,26 @@ class PeerAwareEurekaClusterIntegSpec extends Specification {
         TimeUnit.SECONDS.sleep(90)
 
         given().when()
-                    .accept(ContentType.JSON)
-                    .get("$peer1/apps")
+                .accept(ContentType.JSON)
+                .get("$peer1/apps")
                 .then()
-                    .statusCode(200)
-                    .body("applications.application.name", hasItem("EUREKASERVICE"))
-                    .body("applications.application.instance.app", hasItems(["EUREKASERVICE", "EUREKASERVICE"]))
+                .statusCode(200)
+                .body("applications.application.name", hasItem("EUREKASERVICE"))
+                .body("applications.application.instance.app", hasItems(["EUREKASERVICE", "EUREKASERVICE"]))
 
         and:
         given().when()
-                    .accept(ContentType.JSON)
-                    .get("$peer2/apps")
+                .accept(ContentType.JSON)
+                .get("$peer2/apps")
                 .then()
-                    .statusCode(200)
-                    .body("applications.application.name", hasItem("EUREKASERVICE"))
-                    .body("applications.application.instance.app", hasItems(["EUREKASERVICE", "EUREKASERVICE"]))
+                .statusCode(200)
+                .body("applications.application.name", hasItem("EUREKASERVICE"))
+                .body("applications.application.instance.app", hasItems(["EUREKASERVICE", "EUREKASERVICE"]))
+
+        where: // for readable fail
+        peer1                                | peer2
+        System.getenv("EUREKASERVICE_URI_1") | System.getenv("EUREKASERVICE_URI_2")
+
     }
 
 }
